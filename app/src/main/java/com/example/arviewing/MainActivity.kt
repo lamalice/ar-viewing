d7bc5b914fbddf13385800283ee3d6339ba8a879
Alice <lamalice9496@gmail.com>
2023-06-08 21:01:40 -0400

    added cake 3d model to viewing screen with camera

--- app/src/main/java/com/example/arviewing/MainActivity.kt
+++ app/src/main/java/com/example/arviewing/MainActivity.kt
@@ -7,12 +7,17 @@ import android.os.Bundle
 import com.example.arviewing.databinding.ActivityMainBinding
 
 import io.github.sceneview.ar.ArSceneView
+import io.github.sceneview.ar.arcore.LightEstimationMode
+import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.ar.node.PlacementMode
+import io.github.sceneview.math.Position
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var arSceneView: ArSceneView? = null
+    private lateinit var modelNode: ArModelNode
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +26,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         arSceneView = binding.arSceneView
+
+        arSceneView = binding.arSceneView.apply {
+            lightEstimationMode = LightEstimationMode.ENVIRONMENTAL_HDR_NO_REFLECTIONS
+        }
+
+        modelNode = ArModelNode(PlacementMode.INSTANT).apply {
+            loadModelGlbAsync(
+                glbFileLocation = "models/handpainted_watercolor_cake.glb",
+                scaleToUnits = 1f,
+                centerOrigin = Position(-0.5f)
+            )
+            {
+                arSceneView!!.planeRenderer.isVisible = true
+            }
+        }
+        arSceneView!!.addChild(modelNode)
+        modelNode.anchor()
+        arSceneView!!.planeRenderer.isVisible = false
 
     }
 
